ecf0076666e894a09ae99c57319667c977df62d3
bharathapun <40815908+bharathapun@users.noreply.github.com>
2018-07-17 09:50:10 +0530

    Update h.py

--- h.py
+++ h.py
@@ -1,9 +1,25 @@
-x=int(input())
-if(x%400==0):
-    print ("leapyear")
-elif(x%4==0):
-    print ("leapyear")
-elif(x%100!=0):
-    print ("leapyear")
-else:
-    print ("not leapyear")
+import java.util.Scanner;
+class Leap
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if((n%4==0) && (n%100!=0))
+		{
+		System.out.println("leap year");
+		}
+		else if(n%100==0)
+		{
+		System.out.println("Not leap year");
+		}
+		else if(n%400==0)
+		{
+		System.out.println("leap year");
+		}
+		else
+		{
+		System.out.println("Not leap year");
+		}
+	}
+}
